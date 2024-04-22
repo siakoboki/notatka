4e6853e28340ec217ea9cd9aeba202a9885e428a
Slawomir Zablocki <slawekzablocki@op.pl>
2024-04-22 15:19:15 +0200

    Drugi commit

--- actions/api/test.js
+++ actions/api/test.js
@@ -2,4 +2,7 @@ module.exports = {
 	homepage: function (req, res) {
 		res.send("Strona główna działa !");
 	},
+	kontakt: function (req, res) {
+		res.send("Kontakty strony !!!!");
+	},
 };
--- routes/api.js
+++ routes/api.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const testActions = require("../actions/api/test");
 
 router.get("/", testActions.homepage);
+router.get("/kontakt", testActions.kontakt);
 
 module.exports = router;
