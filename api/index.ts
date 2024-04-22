1dee98abeb8bc7c769dceee7a29e13ec69b699d2
Slawomir Zablocki <slawekzablocki@op.pl>
2024-04-22 17:58:27 +0200

    first commit

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-const { port } = require("./config");
-const apiRouter = require("./routes/api");
+const { port } = require("../config");
+const apiRouter = require("../routes/api");
 
 // routes
 app.use("/", apiRouter);
@@ -10,3 +10,4 @@ app.use("/", apiRouter);
 app.listen(port, function () {
 	console.log("serwer słucha ... http://localhost:" + port);
 });
+module.exports = app;
